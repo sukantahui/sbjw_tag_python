620804ac22951b31e254bb3bbf343d47192ac075
sukanta hui <huisukanta77@gmail.com>
2020-08-12 10:21:11 +0530

    frame with grid added

--- mainFile.py
+++ mainFile.py
@@ -1,4 +1,6 @@
 import temp
+from tkinter import *
+from tkinter import ttk
 # working
 import mysql.connector
 import time
@@ -10,6 +12,7 @@ try:
 
     # load mysql connector        python -m pip install mysql-connector-python
     connection = databse.databaseConnection()
+
     if connection.is_connected():
         db_Info = connection.get_server_info()
         print("Connected to MySQL Server version ", db_Info)
@@ -29,7 +32,7 @@ try:
             f.write("This is line %d\r\n" % (i + 1))
         f.close()
         temp.print_world()
-        time.sleep(5)
+        # time.sleep(5)
 except Error as e:
     # print("Error while connecting to MySQL",e)
     msg = 'Failure in connecting to database. Error: {0}'.format(e)
@@ -41,3 +44,53 @@ except Error as e:
 #     cursor.close()
 #     connection.close()
 #     print("MySQL connection is closed")
+
+print("Working area")
+root = Tk()
+# root.geometry("1350x650")
+root.resizable(width=FALSE, height=FALSE)
+root.geometry('{}x{}'.format(460, 350))
+
+# create all of the main containers
+top_frame = Frame(root, bg='cyan', width=450, height=50, pady=3)
+center = Frame(root, bg='gray2', width=50, height=40, padx=3, pady=3)
+btm_frame = Frame(root, bg='white', width=450, height=45, pady=3)
+btm_frame2 = Frame(root, bg='lavender', width=450, height=60, pady=3)
+
+# layout all of the main containers
+root.grid_rowconfigure(1, weight=1)
+root.grid_columnconfigure(0, weight=1)
+
+top_frame.grid(row=0, sticky="ew")
+center.grid(row=1, sticky="nsew")
+btm_frame.grid(row=3, sticky="ew")
+btm_frame2.grid(row=4, sticky="ew")
+
+# create the widgets for the top frame
+model_label = Label(top_frame, text='Model Dimensions')
+width_label = Label(top_frame, text='Width:')
+length_label = Label(top_frame, text='Length:')
+entry_W = Entry(top_frame, background="pink")
+entry_L = Entry(top_frame, background="orange")
+
+# layout the widgets in the top frame
+model_label.grid(row=0, columnspan=3)
+width_label.grid(row=1, column=0)
+length_label.grid(row=1, column=2)
+entry_W.grid(row=1, column=1)
+entry_L.grid(row=1, column=3)
+
+# create the center widgets
+center.grid_rowconfigure(0, weight=1)
+center.grid_columnconfigure(1, weight=1)
+
+ctr_left = Frame(center, bg='blue', width=100, height=190)
+ctr_mid = Frame(center, bg='yellow', width=250, height=190, padx=3, pady=3)
+ctr_right = Frame(center, bg='green', width=100, height=190, padx=3, pady=3)
+
+ctr_left.grid(row=0, column=0, sticky="ns")
+ctr_mid.grid(row=0, column=1, sticky="nsew")
+ctr_right.grid(row=0, column=2, sticky="ns")
+
+
+mainloop()
