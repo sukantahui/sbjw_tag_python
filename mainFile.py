2484c89579378743997db55d56d2fa44f9305be2
sukanta hui <huisukanta77@gmail.com>
2020-08-12 12:47:15 +0530

    some changes

--- mainFile.py
+++ mainFile.py
@@ -52,8 +52,8 @@ root.resizable(width=FALSE, height=FALSE)
 root.geometry('{}x{}'.format(1350, 650))
 
 # create all of the main containers
-top_frame = Frame(root, bg='cyan', width=1350, height=350, pady=3)
-center = Frame(root, bg='gray2', width=1350, padx=3, pady=3)
+top_frame = Frame(root, bg='cyan', pady=3, padx=50)
+center_frame = Frame(root, bg='gray2', padx=3, pady=3)
 btm_frame = Frame(root, bg='white', width=1350, height=45, pady=3)
 btm_frame2 = Frame(root, bg='green', width=1350, height=60, pady=3)
 
@@ -62,7 +62,7 @@ root.grid_rowconfigure(1, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
 top_frame.grid(row=0, sticky="ew")
-center.grid(row=1, sticky="nsew")
+center_frame.grid(row=1, sticky="nsew")
 btm_frame.grid(row=3, sticky="ew")
 btm_frame2.grid(row=4, sticky="ew")
 
@@ -81,16 +81,18 @@ entry_W.grid(row=1, column=1)
 entry_L.grid(row=1, column=3)
 
 # create the center widgets
-center.grid_rowconfigure(0, weight=1)
-center.grid_columnconfigure(1, weight=1)
+center_frame.grid_rowconfigure(0, weight=1)
+center_frame.grid_columnconfigure(1, weight=1)
 
-ctr_left = Frame(center, bg='blue', width=300, height=190)
-ctr_mid = Frame(center, bg='yellow', width=250, height=190, padx=3, pady=3)
-ctr_right = Frame(center, bg='green', width=100, height=190, padx=3, pady=3)
+ctr_left = Frame(center_frame, bg='blue', width=100)
+ctr_mid = Frame(center_frame, bg='yellow', padx=3, pady=3)
+ctr_right = Frame(center_frame, bg='green', width=100, padx=3, pady=3)
 
 ctr_left.grid(row=0, column=0, sticky="ns")
 ctr_mid.grid(row=0, column=1, sticky="nsew")
 ctr_right.grid(row=0, column=2, sticky="ns")
+
+
 
 
 mainloop()
